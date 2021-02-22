4374be31bc065498b699023d71a010e7dc5ebdfb
sherzodbek-ortiq <v-sortik@microsoft.com>
2021-02-22 15:49:30 +0300

    Adding the first type of bar on the bottom of AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
         ),
-        home: CategoriesScreen(),
+        home: TabsScreen(),
         routes: {
           CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
           MealDetailScreen.routeName: (context) => MealDetailScreen(),
