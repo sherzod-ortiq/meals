e88d93d212f3ee02ed304de30096b86118ad173c
sherzodbek-ortiq <v-sortik@microsoft.com>
2021-02-23 14:59:27 +0300

    Implementing filters

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './models/meal.dart';
+import './dummy_data.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -10,7 +12,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,9 +75,11 @@ class MyApp extends StatelessWidget {
         initialRoute: '/',
         routes: {
           '/': (context) => TabsScreen(),
-          CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+          CategoryMealsScreen.routeName: (context) =>
+              CategoryMealsScreen(_availableMeals),
           MealDetailScreen.routeName: (context) => MealDetailScreen(),
-          FiltersScreen.routeName: (ciontext) => FiltersScreen(),
+          FiltersScreen.routeName: (ciontext) =>
+              FiltersScreen(_setFilters, _filters),
         },
         onUnknownRoute: (settings) {
           return MaterialPageRoute(
